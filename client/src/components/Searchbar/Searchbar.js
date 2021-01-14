d4248507586441f4c49ea93e0004da25d0ce6c5e
Maria Waslick <mariawaslick@gmail.com>
2021-01-14 17:51:57 -0500

    worked on writing function for gathering book info

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-export default function Searchbar() {
+export default function Searchbar(props) {
     return(
         <Form>
             <Form.Label>Search for a book</Form.Label>
-                <Form.Control type="text" placeholder="Enter text" id="booksearch" />
+                <Form.Control type="text" placeholder="Enter text" value={props.searchTerm} onChange={props.handleChange} />
 
             
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={props.searchFunction}>
                     Submit
             </Button>
         </Form>
--- client/src/pages/BookSearch.js
+++ client/src/pages/BookSearch.js
@@ -1,11 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import API from '../utils/apiroutes';
 import Searchbar from '../components/Searchbar/Searchbar'
+import Container from 'react-bootstrap/Container'
 
 export default function BookSearch() {
 
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleChange = event => {
+        setSearchTerm(event.target.value)
+    };
+
+    const searchFunction = event => {
+        event.preventDefault();
+        if (!searchTerm) {
+            console.log("no search term")
+            return
+        }
+        else API.searchBooks(searchTerm)
+        .then (results => {
+           let searchResults = [];
+           results.data.items.forEach(item => {
+               if (typeof item.volumeInfo.categories != undefined) {
+                   console.log(item)
+    
+               }
+           })
+        //    console.log(searchResults)
+        //    setSearchResults(searchResults);
+        //    return searchResults;
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+
     return (
-        <Searchbar />
+        <Container>
+             <Searchbar
+             searchTerm= {searchTerm}
+             handleChange= {handleChange} 
+             searchFunction= {searchFunction}
+             />
+
+
+        </Container>
+       
 
     )
 }
